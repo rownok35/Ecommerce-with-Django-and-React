9133fa0dbb0f2f2e17aed2544588a7b0f32447cb
rownok35 <rownokrezwan@gmail.com>
2022-05-11 18:50:57 +0600

    Database Setup and Admin Panel

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -128,3 +129,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS: True
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    
+]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
