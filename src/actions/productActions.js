d4478f911e1a6f36612f350d369d91f59e02c90a
rownok35 <rownokrezwan@gmail.com>
2022-05-12 13:03:22 +0600

    Section 4: Creating Product Detail Reducer & Action

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -43,3 +43,24 @@ export const listProducts = () => async (dispatch) => {
     });
   }
 };
+
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+
+    const { data } = await axios.get(`/api/products/${id}`);
+
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    });
+  }
+};
