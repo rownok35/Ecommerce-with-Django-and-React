a1ae802bb71dff347fc8ed57ae96121208e202c2
rownok35 <rownokrezwan@gmail.com>
2022-05-15 11:31:50 +0600

    section 6: Register Users

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('users/profile/', views.getUserProfile, name='users-profile'),
     path('users/', views.getUsers, name='users'),
-    path('', views.getRoutes, name ="routes"),
+    path('users/register/', views.registerUser, name='register'),
     path('products/', views.getProducts, name ="products"),
     path('products/<str:pk>', views.getProduct, name ="product"),
     
